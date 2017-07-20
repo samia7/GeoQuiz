c6e9287dcb9906d450a25c451e88f2db4eee285b
Samia Sama <samia.ss@live.com>
2017-07-20 17:45:30 +0600

    Add override methods for lifestyle

--- app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
+++ app/src/main/java/geoquiz/android/bignerdranch/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package geoquiz.android.bignerdranch.com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG= "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -26,8 +28,15 @@ public class QuizActivity extends AppCompatActivity {
     private int mCurrentIndex=0;
 
     @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView=(TextView) findViewById(R.id.question_text_view);
@@ -57,6 +66,30 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
     }
 
     private void updateQuestion() {
